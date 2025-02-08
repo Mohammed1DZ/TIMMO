6f3f1c8610883e4fb4778dd737accfca2e752277
Mohammed1DZ <mohammedfreehali@gmail.com>
2025-02-08 20:04:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Dashboard from './pages/Dashboard';
@@ -8,11 +8,24 @@ import Agents from './pages/Agents';
 import Settings from './pages/Settings';
 
 function App() {
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+
     return (
         <Router>
             <div className="flex">
-                <Sidebar />
-                <div className="flex-1 p-10 bg-gray-100 min-h-screen">
+                {/* Sidebar */}
+                <Sidebar isOpen={sidebarOpen} toggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
+
+                {/* Mobile menu button */}
+                <button
+                    className="lg:hidden p-4 bg-blue-700 text-white fixed top-0 left-0 z-20"
+                    onClick={() => setSidebarOpen(!sidebarOpen)}
+                >
+                    Menu
+                </button>
+
+                {/* Main content */}
+                <div className="flex-1 p-10 bg-gray-100 min-h-screen ml-0 lg:ml-64">
                     <Routes>
                         <Route path="/" element={<Dashboard />} />
                         <Route path="/properties" element={<Properties />} />
