65bad6aab27a411b48800bd1a6d928b70a9705ab
Mohammed1DZ <mohammedfreehali@gmail.com>
2025-02-08 20:36:48 +0100

    Update Properties.js

--- src/pages/Properties.js
+++ src/pages/Properties.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import PropertyForm from '../components/PropertyForm';
 
 const Properties = () => {
     return (
-        <div>
-            <h1>Properties Page</h1>
-            <p>This is the properties page where you can view and manage properties.</p>
+        <div className="p-10">
+            <h1 className="text-3xl font-bold mb-6">Properties Management</h1>
+            <p className="mb-6">This is the properties page where you can view and manage properties.</p>
+            
+            {/* Property Form */}
+            <PropertyForm />
         </div>
     );
 };
