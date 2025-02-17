e7db6b8db3eb95bd3660aeff93e741a231ba7b6c
Mohammed1DZ <mohammedfreehali@gmail.com>
2025-02-17 01:50:33 +0100

    Delete src directory

--- src/utils/tokenUtils.js
+++
@@ -1,6 +0,0 @@
-export const isTokenExpired = () => {
-    const tokenExpiry = localStorage.getItem('tokenExpiry');
-    if (!tokenExpiry) return true;
-
-    return new Date().getTime() > parseInt(tokenExpiry);
-};
